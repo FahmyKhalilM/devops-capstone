69f391f45c5899d4e3b697c8963af9b0b935a368
FahmyKhalilM <72336661+FahmyKhalilM@users.noreply.github.com>
2022-09-15 13:38:56 +0200

    Update build_docker.sh

--- bin/build_docker.sh
+++ bin/build_docker.sh
@@ -2,7 +2,10 @@
 
 CONTAINER_NAME="hello-app"
 VERSION=1.000
+dockerpath="fahmykhalilm/cap_project"
 
 # Step 1:
 # Build image and add a descriptive tag
 docker build --tag ${CONTAINER_NAME}:${VERSION} hello_app
+docker build --tag=hello_app
+docker tag hello_app:latest $dockerpath:hello_app
